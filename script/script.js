40859a3080e673417a52572b0ea84bda2974c539
Dikontay <dalimbayeva@icloud.com>
2023-10-21 02:47:06 +0600

    add event listener

--- script/script.js
+++ script/script.js
@@ -1,14 +1,30 @@
+
+const searchInput = document.getElementById('searchbar');
+searchInput.addEventListener('input', search_course);
 function search_course() { 
     let input = document.getElementById('searchbar').value.toLowerCase();
-    let courseElements = document.querySelectorAll('.main-courses .card');
+    let courseElements = document.querySelectorAll('.container .card');
+    let searchResults = document.getElementById('search-results');
 
-    courseElements.forEach(courseElement => {
+    while (searchResults.firstChild) {
+        searchResults.removeChild(searchResults.firstChild);
+    }
+
+ courseElements.forEach(courseElement => {
         let title = courseElement.querySelector('.card-title').textContent.toLowerCase();
+            //console.log(title)
+        
+        // Check if the input exists in the title of this course element
         if (title.includes(input)) {
-            console.log(courseElement)
-            courseElement.style.display = "block"; // Show the course
+          
+            // This course matches the search criteria
+            const matchingCardCopy = courseElement
+
+            searchResults.appendChild(matchingCardCopy);
+           
         } else {
-            courseElement.style.display = "none"; // Hide the course
+            // This course does not match the search criteria, hide it
+            courseElement.style.display = 'none';
         }
     });
-} 
+}
