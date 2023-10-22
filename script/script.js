3208fd8feeba28b7b9129db17ec861e89822720e
Dikontay <118036214+Dikontay@users.noreply.github.com>
2023-10-22 15:02:49 +0600

    add timer

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-//search
 function searchCourse() {
-
+    // Get the search term from the input field
     const searchTerm = document.getElementById("searchbar").value.toLowerCase();
 
-
+    // Get all the course cards
     const courses = document.querySelectorAll(".card");
 
-
+    // Get the search results container
     const foundCardsContainer = document.getElementById("found-cards-container");
 
+    // Show the "Not Found" message by default
     const notFoundMessage = document.getElementById("not-found-message");
     notFoundMessage.style.display = "block";
 
-
+    // Clear the centered container
     foundCardsContainer.innerHTML = '';
 
-
+    // Loop through the cards and check if the course title includes the search term
     let anyCardFound = false;
 
-
+    // Display all cards initially
     courses.forEach((course) => {
         course.style.display = "block";
     });
@@ -27,10 +27,10 @@ function searchCourse() {
         const courseTitle = course.querySelector(".card-title").textContent.toLowerCase();
 
         if (courseTitle.includes(searchTerm)) {
-
+            // If it matches, move the card to the centered container
             foundCardsContainer.appendChild(course);
 
-
+            // Hide the "Not Found" message
             notFoundMessage.style.display = "none";
 
             anyCardFound = true;
@@ -39,12 +39,12 @@ function searchCourse() {
         }
     });
 
-
+    // Display the centered container if any card is found
     if (anyCardFound) {
         foundCardsContainer.style.display = "block";
     }
 }
-//form validation
+
 function validateForm() {
     const firstName = document.forms["myForm"]["firstName"].value;
     const lastName = document.forms["myForm"]["lastName"].value;
@@ -55,31 +55,19 @@ function validateForm() {
 
     if (firstName === "" || lastName === "" || email === "" || phone === "" || reason === "" || !agreement) {
         alert("Please fill out all required fields and agree to the terms.");
-        return false;
+        return false; // Prevent form submission
     }
 
+    // Additional validation checks can be added for specific fields (e.g., email format, phone number format).
 
+    // If all validation passes, you can submit the form
     alert("Form submitted successfully!");
-    return true;
+    return true; // Allow form submission
 }
 
-//image slider
-let  currentSlide = 0;
-const slides = document.querySelectorAll('#slider img');
+setInterval(myTimer, 1000);
 
-function changeSlide(direction) {
-    slides[currentSlide].classList.remove('d-block');
-    slides[currentSlide].classList.add('d-none');
-    currentSlide += direction;
-
-    if (currentSlide < 0) {
-        currentSlide = slides.length - 1;
-    }
-
-    if (currentSlide >= slides.length) {
-        currentSlide = 0;
-    }
-
-    slides[currentSlide].classList.remove('d-none');
-    slides[currentSlide].classList.add('d-block');
+function myTimer() {
+    const d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 }
